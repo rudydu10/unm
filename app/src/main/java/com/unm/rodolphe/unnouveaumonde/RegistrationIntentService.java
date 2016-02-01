26db2c58a28a76c52cad2de90b73b044f0c2cb2c
rudydu10 <riviere.rodolphe@hotmail.fr>
2016-02-01 07:55:38 +0100

    Mise en place de Google Cloud Message (notification push)

--- app/src/main/java/com/unm/rodolphe/unnouveaumonde/RegistrationIntentService.java
+++ app/src/main/java/com/unm/rodolphe/unnouveaumonde/RegistrationIntentService.java
@@ -35,12 +35,10 @@ public class RegistrationIntentService extends IntentService {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         try {
-            // In the (unlikely) event that multiple refresh operations occur simultaneously,
-            // ensure that they are processed sequentially.
+
             synchronized (TAG) {
-                // [START register_for_gcm]
-                // Initially this call goes out to the network to retrieve the token, subsequent calls
-                // are local.
+                // [DEMARRER enregistrement pour gcm]
+
                 InstanceID instanceID = InstanceID.getInstance(this);
 
                 String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
@@ -52,16 +50,12 @@ public class RegistrationIntentService extends IntentService {
                 if (!sharedPreferences.getBoolean(Constants.SENT_TOKEN_TO_SERVER, false))
                     sendRegistrationToServer(token);
 
-                    // You should store a boolean that indicates whether the generated token has been
-                    // sent to your server. If the boolean is false, send the token to your server,
-                    // otherwise your server should have already received the token.
                     sharedPreferences.edit().putBoolean(Constants.SENT_TOKEN_TO_SERVER, true).apply();
 
             }
         } catch (Exception e) {
-            Log.d(TAG, "Failed to complete token refresh", e);
-            // If an exception happens while fetching the new token or updating our registration data
-            // on a third-party server, this ensures that we'll attempt the update at a later time.
+            Log.d(TAG, "Echec de renouvellement du Token", e);
+
             sharedPreferences.edit().putBoolean(Constants.SENT_TOKEN_TO_SERVER, false).apply();
         }
         // Notify UI that registration has completed, so the progress indicator can be hidden.
@@ -69,11 +63,7 @@ public class RegistrationIntentService extends IntentService {
         LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
     }
 
-    /**
-     *  Ici nous allons envoyer le token de l'utilisateur au serveur
-     *
-     * @param token Le token
-     */
+
     private void sendRegistrationToServer(String token) {
 
         OkHttpClient client = new OkHttpClient();
